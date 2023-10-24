5ddc5966a8064a8cfc079d9e3869973e65993819
Rdnbt <haroldbudd12@gmail.com>
2023-10-25 02:57:34 +0900

    Set up translation checker application

--- app/main.py
+++ app/main.py
@@ -15,58 +15,63 @@ def load_corrected_dataset(data_dir, selected_part):
 
 st.title("Translation Checker App")
 
-selected_part = st.selectbox("Select the part:", [i for i in range(1, 33)])
+# Sidebar for file and sentence number selection
+selected_part = st.sidebar.selectbox("Select the part:", [i for i in range(1, 33)])
 
 data_dir = '../data/Translated/'
-df = load_dataset(data_dir, selected_part)
+df_original = load_dataset(data_dir, selected_part)
 
 corrected_dir = '../data/corrected/'
 corrected_df = load_corrected_dataset(corrected_dir, selected_part)
 
 if corrected_df is not None:
-    df = df.merge(corrected_df[['image_name', 'comment_number', 'Translated Comment']], on=['image_name', 'comment_number'], how='left', suffixes=('', '_corrected'))
+    df = corrected_df
+else:
+    df = df_original.copy()
+    df['corrected_comment'] = None
 
-if 'index' not in st.session_state:
-    st.session_state.index = 0
+# Display sentence numbers along with correction status for selection
+sentence_numbers = [f"{num + 1} - {'Corrected' if not pd.isna(df.iloc[num]['corrected_comment']) else 'Not Corrected'}" for num in range(len(df))]
 
-index = st.session_state.index
+# Sidebar selector for sentence number
+selected_sentence_number = st.sidebar.selectbox("Select the sentence number:", sentence_numbers)
 
-# Display the image
+# Extract the sentence number from the selected option
+selected_sentence_number = int(selected_sentence_number.split(" - ")[0])
+
+# Determine the index based on the selected sentence number
+index = selected_sentence_number - 1
+
+# Display the image corresponding to the selected sentence
 st.image(os.path.join('/Users/erdnbt/Projects/Research/flickr30k_images/flickr_images', df.iloc[index]['image_name'].strip()))
 
-# Displaying the original caption and corrected/machine translations
+# Displaying the original caption
 st.write(f"Original: {df.iloc[index]['comment'].strip()}")
 
-if 'Translated Comment_corrected' in df.columns and pd.notna(df.iloc[index]['Translated Comment_corrected']):
-    corrected_text_color = 'green'
-    corrected_translation_display = df.iloc[index]['Translated Comment_corrected'].strip()
+# Check for the existence of 'Translated Comment' column before accessing
+if 'Translated Comment' in df.columns:
+    machine_translation = df.iloc[index]['Translated Comment'].strip()
 else:
-    corrected_text_color = 'red'
-    corrected_translation_display = df.iloc[index]['Translated Comment'].strip()
+    machine_translation = ""
 
-st.markdown(f"**Translation:** <span style='color: {corrected_text_color};'>{corrected_translation_display}</span>", unsafe_allow_html=True)
+# Display machine translation
+st.write(f"Machine Translation: {machine_translation}")
 
-corrected_translation = st.text_area("Correct the translation:", df.iloc[index]['Translated Comment'].strip())
+# Displaying the corrected translation if it exists
+corrected_comment = df.iloc[index]['corrected_comment'] if 'corrected_comment' in df.columns else machine_translation
 
-# Buttons: Previous, Submit, Next
-col1, col2, col3 = st.columns(3)
+if not pd.isna(corrected_comment):
+    st.markdown(f"**Corrected Translation (green):** <span style='color: green;'>{corrected_comment.strip()}</span>", unsafe_allow_html=True)
 
-with col1:
-    if st.button("Previous") and index > 0:
-        st.session_state.index -= 1
+# Text area and Submit button
+corrected_translation = st.text_area(
+    "Correct the translation:",
+    corrected_comment
+)
 
-with col2:
-    if st.button("Submit"):
-        # Update the dataframe with the latest correction
-        df.at[index, 'Translated Comment_corrected'] = corrected_translation.strip()
-
-        # Extract only the necessary columns and rename as needed
-        corrected_df = df[['image_name', 'comment_number', 'Translated Comment_corrected']]
-        corrected_df = corrected_df.rename(columns={"Translated Comment_corrected": "Translated Comment"})
-
-        # Save the dataframe back to the corrected file
-        corrected_df.to_csv(os.path.join('../data/corrected/', f'flickr30k_part_{selected_part}_corrected.csv'), sep='|', index=False)
-
-with col3:
-    if st.button("Next") and index < len(df) - 1:
-        st.session_state.index += 1
+if st.button("Submit"):
+    # Update the dataframe with the latest correction
+    df.at[index, 'corrected_comment'] = corrected_translation.strip()
+    
+    # Save the dataframe back to the corrected file
+    df[['image_name', 'comment_number', 'comment', 'corrected_comment']].to_csv(os.path.join('../data/corrected/', f'flickr30k_part_{selected_part}_corrected.csv'), sep='|', index=False)
